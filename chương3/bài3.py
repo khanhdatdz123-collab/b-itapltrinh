7d55867f848d955760ce086f02c8b2b2482fa4da
khanhdatdz123-collab <khanhdatdz123@gmail.com>
2025-11-27 19:31:11 +0700

    Add score grading functionality in bài3.py

---
+++ chương3/bài3.py
@@ -0,0 +1,19 @@
+score = input("Enter score: ")
+try:
+    score = float(score)
+except:
+    print("Bad score")
+if score < 0.0:
+    print("Bad score")
+elif score > 1.0:
+    print("Bad score")
+elif score >= 0.9:
+    print("A")
+elif score >= 0.8:
+    print("B")
+elif score >= 0.7:
+    print("C")
+elif score >= 0.6:
+    print("D")
+else:
+    print("F")
