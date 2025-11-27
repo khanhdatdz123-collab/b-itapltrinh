7bb86d3a00b84518fd9c13e673bc2af93382e70e
khanhdatdz123-collab <khanhdatdz123@gmail.com>
2025-11-27 19:26:20 +0700

    Add bài3.py with basic arithmetic operations

---
+++ chương2/bài3.py
@@ -0,0 +1,6 @@
+width = 17
+heigth =12.0
+print("1. width//2 = ", width//2, type(width//2))
+print("2. width/2 = ", width/2, type(width/2))
+print("3. heigth/3 = ", heigth/3, type(heigth/3))
+print("4. 1+2*5 = ", 1+2*5, type(1+2*5))
