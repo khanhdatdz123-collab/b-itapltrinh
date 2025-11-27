750ef8d0c19be12f936c325f2bf701540428ee09
khanhdatdz123-collab <khanhdatdz123@gmail.com>
2025-11-27 19:37:27 +0700

    Add script to count subject lines in a file

---
+++ chương7/bài3.py
@@ -0,0 +1,14 @@
+fname = input("Enter the file name: ")
+if fname == "na na boo boo":
+    print ("NA NA BOOBOO TO YOU = You have been punk'd!")
+    quit ()
+try:
+    fhand = open(fname)
+except:
+    print ("File cannot be opened:", fname)
+    quit()
+count = 0
+for line in fhand:
+    if line. startswith("Subject:"):
+        count += 1
+print("There were", count, "subject lines in", name)
